b2d28a0158cd29097cb3bd7dd11173e7ea4ad5bb
Aleksandr Panchul <panchul@users.noreply.github.com>
2021-02-25 16:06:18 -0800

    a demo on COM objects

---
+++ UsingManagedComObjectInC1/MFCApplication1/CUserControl1.h
@@ -0,0 +1,26 @@
+// Machine generated IDispatch wrapper class(es) created with Add Class from Typelib Wizard
+
+#import "..\\WindowsFormsControlLibrary1\\bin\\Debug\\WindowsFormsControlLibrary1.tlb" no_namespace
+// CUserControl1 wrapper class
+
+class CUserControl1 : public COleDispatchDriver
+{
+public:
+	CUserControl1() {} // Calls COleDispatchDriver default constructor
+	CUserControl1(LPDISPATCH pDispatch) : COleDispatchDriver(pDispatch) {}
+	CUserControl1(const CUserControl1& dispatchSrc) : COleDispatchDriver(dispatchSrc) {}
+
+	// Attributes
+public:
+
+	// Operations
+public:
+
+
+	// _UserControl1 methods
+public:
+
+	// _UserControl1 properties
+public:
+
+};
---
+++ UsingManagedComObjectInC1/Readme.md
@@ -0,0 +1,8 @@
+ 
+A sandbox for COM objects and C++/C# interoperability.
+
+https://stackoverflow.com/questions/12429282/using-a-managed-com-object-in-c
+
+https://www.red-gate.com/simple-talk/dotnet/visual-studio/build-and-deploy-a-.net-com-assembly/
+
+ 
