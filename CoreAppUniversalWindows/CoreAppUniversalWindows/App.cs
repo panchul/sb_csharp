1d7164cd09a358445d2d29b55d70920c56562ed4
Aleksandr Panchul <panchul@users.noreply.github.com>
2021-03-13 21:27:46 -0800

    CoreApp UniversalWindow demo, bare essentials of the core

---
+++ CoreAppUniversalWindows/CoreAppUniversalWindows/App.cs
@@ -0,0 +1,43 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+class App : IFrameworkViewSource, IFrameworkView
+{
+    static void Main()
+    {
+        CoreApplication.Run(new App());
+    }
+
+    public IFrameworkView CreateView()
+    {
+        return this;
+    }
+
+    public void SetWindow(CoreWindow window)
+    {
+
+    }
+
+    public void Run()
+    {
+        CoreWindow window = CoreWindow.GetForCurrentThread();
+        window.Activate();
+
+        CoreDispatcher dispatcher = window.Dispatcher;
+        dispatcher.ProcessEvents(CoreProcessEventsOption.ProcessUntilQuit);
+    }
+
+    public void Initialize(CoreApplicationView view)
+    {
+
+    }
+
+    public void Load(string args)
+    {
+
+    }
+
+    public void Uninitialize()
+    {
+
+    }
+}
